fb30e5d85b0677611cba1332a179fdfb12468b7e
Martina Selldén <martina.sellden@gmail.com>
2023-08-23 14:54:50 +0200

    Created layers

---
+++ Labb Clean Code/ConsoleIO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_Clean_Code
+{
+    internal class ConsoleIO : IUI
+    {
+        public void Clear()
+        {
+            //not used;
+        }
+
+        public void Exit()
+        {
+            System.Environment.Exit(0);
+        }
+
+        public string GetString()
+        {
+            return Console.ReadLine();
+        }
+
+        public void PutString(string outputString)
+        {
+            Console.WriteLine(outputString);
+        }
+    }
+}
---
+++ Labb Clean Code/IUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_Clean_Code
+{
+    internal interface IUI
+    {
+        void PutString(string outputString);
+        string GetString();
+        void Exit();
+        void Clear();
+    }
+}
