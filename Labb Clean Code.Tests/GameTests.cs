b745b31eaded0a75efa24dd0d2be9a93709c341b
Martina Selldén <martina.sellden@gmail.com>
2023-09-06 14:54:35 +0200

    Added TestInitialize to set up for Testing in GameTests

--- Labb Clean Code.Tests/GameTests.cs
+++ Labb Clean Code.Tests/GameTests.cs
@@ -10,15 +10,21 @@ namespace Labb_Clean_Code.Tests
     [TestClass]
     public class GameTests
     {
-        [TestMethod]
-        public void TestGenerateRandomNumber()
+        private Game game;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
             IUI ui = new ConsoleIO();
             GameController gameController = new GameController(ui);
             IDataHandler fileHandler = new FileHandler();
             GameScore gameScore = new GameScore(ui);
-            Game game = new Game(ui, gameController, gameScore, fileHandler);
+            game = new Game(ui, gameController, gameScore, fileHandler);
+        }
 
+            [TestMethod]
+        public void TestGenerateRandomNumber()
+        {  
             IRandomNumberGenerator randomNumberGenerator = new MockRandomNumberGenerator(1234);
             string generatedNumber = game.GenerateRandomNumber(randomNumberGenerator);
 
